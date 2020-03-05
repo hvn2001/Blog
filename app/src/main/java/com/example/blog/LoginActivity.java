950ce15bf59f72e96a524c7e92b4bb1a21bf4794
HoangVu <hoangvu1991uit@gmail.com>
2020-03-05 22:53:35 +0700

    Validation & error message

--- app/src/main/java/com/example/blog/LoginActivity.java
+++ app/src/main/java/com/example/blog/LoginActivity.java
@@ -1,14 +1,44 @@
 package com.example.blog;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private TextInputLayout textUsernameLayout;
+    private TextInputLayout textPasswordInput;
+    private Button loginButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textUsernameLayout = findViewById(R.id.textUsernameLayout);
+        textPasswordInput = findViewById(R.id.textPasswordInput);
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginActivity.this.onLoginClicked();
+            }
+        });
+        // loginButton.setOnClickListener(v -> onLoginClicked());
+    }
+
+    private void onLoginClicked() {
+        String username = textUsernameLayout.getEditText().getText().toString();
+        String password = textPasswordInput.getEditText().getText().toString();
+        if (username.isEmpty()) {
+            textUsernameLayout.setError("Username must not be empty");
+        } else if (password.isEmpty()) {
+            textPasswordInput.setError("Password must not be empty");
+        }
     }
 }
