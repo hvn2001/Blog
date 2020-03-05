dfdc376f3a6c5bd9f9511250c2d83fcb43a1b73f
HoangVu <hoangvu1991uit@gmail.com>
2020-03-05 18:10:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/blog/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.blog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView mainTextView = findViewById(R.id.mainTextView);
+        mainTextView.setText("Hello educative.io");
+    }
+}
