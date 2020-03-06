533ea5058d7167d680714ca9c68b6b8191e459e6
HoangVu <hoangvu1991uit@gmail.com>
2020-03-06 17:17:23 +0700

    access Internet - Image Loading

--- app/src/main/java/com/example/blog/BlogDetailsActivity.java
+++ app/src/main/java/com/example/blog/BlogDetailsActivity.java
@@ -8,7 +8,16 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+
 public class BlogDetailsActivity extends AppCompatActivity {
+
+    public static final String IMAGE_URL =
+            "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
+                    "3436e16367c8ec2312a0644bebd2694d484eb047/images/sydney_image.jpg";
+    public static final String AVATAR_URL =
+            "https://bitbucket.org/dmytrodanylyk/travel-blog-resources/raw/" +
+                    "3436e16367c8ec2312a0644bebd2694d484eb047/avatars/avatar1.jpg";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,10 +26,17 @@ public class BlogDetailsActivity extends AppCompatActivity {
 
         // Setting data from code
         ImageView imageMain = findViewById(R.id.imageMain);
-        imageMain.setImageResource(R.drawable.sydney_image);
+        // imageMain.setImageResource(R.drawable.sydney_image);
+        Glide.with(this)
+                .load(IMAGE_URL)
+                .into(imageMain);
+
 
         ImageView imageAvatar = findViewById(R.id.imageAvatar);
-        imageAvatar.setImageResource(R.drawable.avatar);
+        // imageAvatar.setImageResource(R.drawable.avatar);
+        Glide.with(this)
+                .load(AVATAR_URL)
+                .into(imageAvatar);
 
         TextView textTitle = findViewById(R.id.textTitle);
         textTitle.setText("G'day from Sydney");
