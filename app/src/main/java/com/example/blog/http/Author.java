4dbe96d0cf6a2d47d520b902c18a66aa2db001ae
HoangVu <hoangvu1991uit@gmail.com>
2020-03-06 18:03:12 +0700

    Travel Blog - Details Screen - Data models

---
+++ app/src/main/java/com/example/blog/http/Author.java
@@ -0,0 +1,15 @@
+package com.example.blog.http;
+
+public class Author {
+
+    private String name;
+    private String avatar;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+}
