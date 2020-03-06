c9a015887348a5da124ea93858c79ae472064112
HoangVu <hoangvu1991uit@gmail.com>
2020-03-06 21:23:12 +0700

    Travel Blog - List Screen - Building Layout

--- app/src/main/java/com/example/blog/MainActivity.java
+++ app/src/main/java/com/example/blog/MainActivity.java
@@ -12,10 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView mainTextView = findViewById(R.id.mainTextView2);
-        mainTextView.setText("Hello educative.io");
-
         startActivity(new Intent(this, BlogDetailsActivity.class));
     }
 }
