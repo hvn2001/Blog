381ec8d9dac00b8a545cebdf32ccd3ef8928c01b
HoangVu <hoangvu1991uit@gmail.com>
2020-03-07 11:35:27 +0700

    Pull to Refresh

--- app/src/main/java/com/example/blog/MainActivity.java
+++ app/src/main/java/com/example/blog/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.blog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -20,6 +21,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private MainAdapter adapter;
+    private SwipeRefreshLayout refreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +35,26 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+
+        refreshLayout = findViewById(R.id.refresh);
+        refreshLayout.setOnRefreshListener(this::loadData); // 1
         loadData();
     }
 
+    /**
+     * pull-to-refresh listener via the setOnRefreshListener method (1)
+     * When this listener is triggered, we can simply execute the loadData method to reload the data.
+     * To make the loader indicator stay on the screen while data is loading,
+     * we can use the setRefreshing(true) method (2) and
+     * when the data loading has been completed, we can use the setRefreshing(false) method (3) (4).
+     */
     private void loadData() {
+        refreshLayout.setRefreshing(true); // 2
         BlogHttpClient.INSTANCE.loadBlogArticles(new BlogArticlesCallback() {
             @Override
             public void onSuccess(List<Blog> blogList) {
                 runOnUiThread(() -> {
-                    // TODO show data
+                    refreshLayout.setRefreshing(false); // 3
                     adapter.submitList(blogList);
                 });
             }
@@ -49,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onError() {
                 runOnUiThread(() -> {
+                    refreshLayout.setRefreshing(false); // 4
                     showErrorSnackbar();
                 });
             }
