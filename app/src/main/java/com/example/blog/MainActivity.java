ae65d17cfd6b4e85ec2e81d2b362571911f995b7
HoangVu <hoangvu1991uit@gmail.com>
2020-03-07 23:00:15 +0700

    Offline Support

--- app/src/main/java/com/example/blog/MainActivity.java
+++ app/src/main/java/com/example/blog/MainActivity.java
@@ -17,6 +17,8 @@ import com.example.blog.adapter.MainAdapter;
 import com.example.blog.http.Blog;
 import com.example.blog.http.BlogArticlesCallback;
 import com.example.blog.http.BlogHttpClient;
+import com.example.blog.repository.BlogRepository;
+import com.example.blog.repository.DataFromNetworkCallback;
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.snackbar.Snackbar;
@@ -33,10 +35,14 @@ public class MainActivity extends AppCompatActivity {
 
     private int currentSort = SORT_DATE; // 2
 
+    private BlogRepository repository;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        repository = new BlogRepository(getApplicationContext()); // 1
+
         MaterialToolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setOnMenuItemClickListener(item -> {
             if (item.getItemId() == R.id.sort) {
@@ -53,8 +59,11 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
 
         refreshLayout = findViewById(R.id.refresh);
-        refreshLayout.setOnRefreshListener(this::loadData); // 1
-        loadData();
+        // refreshLayout.setOnRefreshListener(this::loadData); // 1
+        // loadData();
+        refreshLayout.setOnRefreshListener(this::loadDataFromNetwork); // 2
+        loadDataFromDatabase(); // 3
+        loadDataFromNetwork(); // 4
 
         MenuItem searchItem = toolbar.getMenu().findItem(R.id.search); // 1
         SearchView searchView = (SearchView) searchItem.getActionView(); // 2
@@ -68,6 +77,36 @@ public class MainActivity extends AppCompatActivity {
             public boolean onQueryTextChange(String newText) {
                 adapter.filter(newText); // 4
                 return true;
+            }
+        });
+    }
+
+    private void loadDataFromDatabase() {
+        repository.loadDataFromDatabase(blogList -> runOnUiThread(() -> {
+            adapter.setData(blogList);
+            sortData();
+        }));
+    }
+
+    private void loadDataFromNetwork() {
+        refreshLayout.setRefreshing(true); // 1
+
+        repository.loadDataFromNetwork(new DataFromNetworkCallback() { // 2
+            @Override
+            public void onSuccess(List<Blog> blogList) {
+                runOnUiThread(() -> { // 3
+                    adapter.setData(blogList);
+                    sortData();
+                    refreshLayout.setRefreshing(false);
+                });
+            }
+
+            @Override
+            public void onError() {
+                runOnUiThread(() -> {
+                    refreshLayout.setRefreshing(false);
+                    showErrorSnackbar();
+                });
             }
         });
     }
@@ -126,7 +165,8 @@ public class MainActivity extends AppCompatActivity {
         Snackbar snackbar = Snackbar.make(rootView, "Error during loading blog articles", Snackbar.LENGTH_INDEFINITE);
         snackbar.setActionTextColor(getResources().getColor(R.color.orange500));
         snackbar.setAction("Retry", v -> {
-            loadData();
+            // loadData();
+            loadDataFromNetwork();
             snackbar.dismiss();
         });
         snackbar.show();
